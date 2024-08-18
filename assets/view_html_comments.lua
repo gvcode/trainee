4ed343952db76693257d9e721d689646704b107e
ricardo-semiao <ricardo.semiao@outlook.com>
2024-08-18 15:46:06 -0300

    Added custom css and visible html comments to professors

---
+++ assets/view_html_comments.lua
@@ -0,0 +1,13 @@
+function RawBlock(el)
+    local comment = el.text:match("^<!%-%-(.-)%-%->")
+    if comment then
+        return pandoc.Para({pandoc.Strong("Adicionar/Editar:"), pandoc.Space(), pandoc.Str(comment)})
+    end
+end
+
+function RawInline(el)
+    local comment = el.text:match("^<!%-%-(.-)%-%->")
+    if comment then
+        return pandoc.Span({pandoc.Strong("Adicionar/Editar:"), pandoc.Space(), pandoc.Str(comment)})
+    end
+end
