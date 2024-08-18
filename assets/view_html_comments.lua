3ebbadf369b545814f21f94199bdf2bd5b25c584
ricardo-semiao <ricardo.semiao@outlook.com>
2024-08-18 20:09:19 -0300

    Finished first round of comments to the rest of aula's

--- assets/view_html_comments.lua
+++ assets/view_html_comments.lua
@@ -1,12 +1,12 @@
 function RawBlock(el)
-    local comment = el.text:match("^<!%-%-(.-)%-%->")
+    local comment = el.text:match("^<!%-%- *(.-) *%-%->")
     if comment then
         return pandoc.Para({pandoc.Strong("Adicionar/Editar:"), pandoc.Space(), pandoc.Str(comment)})
     end
 end
 
 function RawInline(el)
-    local comment = el.text:match("^<!%-%-(.-)%-%->")
+    local comment = el.text:match("^<!%-%- *(.-) *%-%->")
     if comment then
         return pandoc.Span({pandoc.Strong("Adicionar/Editar:"), pandoc.Space(), pandoc.Str(comment)})
     end
